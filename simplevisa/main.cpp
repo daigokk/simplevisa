6f3ee7b347bad3aea72ef42067d4b56a036c7cb5
dk <daigokk@gmail.com>
2025-10-28 17:51:25 +0900

    Update main.cpp

--- simplevisa/main.cpp
+++ simplevisa/main.cpp
@@ -8,17 +8,18 @@ int main() {
 
     // 1. オフィスに入る（リソースマネージャ初期化）
     viOpenDefaultRM(&defaultRM);
+	int numInstruments = 0;
     for (int i = 0; i < 10; i++) {
         ViSession instr = 0;
 		char addr[256];
-		sprintf_s(addr, "TCPIP0::172.16.145.%d::12345::SOCKET", 140 + i);
+		sprintf_s(addr, "TCPIP0::172.16.145.%d::55555::SOCKET", 140 + i);
         // 2. ドアを開ける（計測器に接続）
         viOpen(defaultRM, addr, VI_NULL, 10, &instr);
         if (instr == 0) {
-            printf("Failed to open instrument at %s\n", addr);
+            printf("%s: Failed to open instrument.\n", addr);
             continue;
 		}
-
+        numInstruments++;
         // 3. 質問する（SCPIコマンドを送信　例えば *IDN?, ACQuire:STATE RUN, AUTOSet EXECute）
         viPrintf(instr, "*IDN?\n");
 
@@ -30,6 +31,7 @@ int main() {
         // 5. さよならする（接続を閉じる）
         viClose(instr);
     }
+	printf("Number of instruments found: %d\n", numInstruments);
     viClose(defaultRM);
 
     return 0;
